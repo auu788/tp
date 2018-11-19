4908c165571afcc0cd151d9c20b23a7b9a0e6465
auu788 <auu788@gmail.com>
2018-11-19 20:47:40 +0100

    Added Reader and Rental classes

---
+++ Assignment-1/BooksLib/Reader.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooksLib
+{
+    // Wykaz
+    public class Reader : Person
+    {
+        public Reader(string firstName, string lastName) : base(firstName, lastName) { }
+    }
+}
---
+++ Assignment-1/BooksLib/Rental.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooksLib
+{
+    // Zdarzenie
+    public class Rental
+    {
+        private Reader Reader;
+        private BookItem BookItem;
+        private DateTime RentalDateStart;
+        private DateTime RentalDateEnd;
+        public Guid Guid { get; private set; }
+
+        public Rental(Reader reader, BookItem bookItem)
+        {
+            this.Reader = reader;
+            this.BookItem = bookItem;
+            this.RentalDateStart = DateTime.Now;
+            this.Guid = new Guid();
+        }
+
+        public Rental(Reader reader, BookItem bookItem, DateTime rentalDateStart, DateTime rentalDateEnd)
+        {
+            this.Reader = reader;
+            this.BookItem = bookItem;
+            this.RentalDateStart = rentalDateStart;
+            this.RentalDateEnd = rentalDateEnd;
+        }
+
+        public void finish()
+        {
+            this.RentalDateEnd = DateTime.Now;
+        }
+
+        public bool isFinished()
+        {
+            if (this.RentalDateEnd != null)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0}\n{1}\nData rozpoczecia wypozyczenia: {2} {3}\nData zakonczenia wypozyczenia: {4} {5}",
+                this.Reader,
+                this.BookItem,
+                this.RentalDateStart.ToLongDateString(),
+                this.RentalDateStart.ToLongTimeString(),
+                this.RentalDateEnd.ToLongDateString(),
+                this.RentalDateEnd.ToLongTimeString());
+        }
+    }
+}
